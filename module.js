411c8c4a83a11e9896a8a72e878613389e83fb04
Sam Holmes <samholmes1337@gmail.com>
2019-01-11 11:48:06 +0000

    Update formatting

--- module.js
+++ module.js
@@ -1,7 +1,7 @@
 /**
  * This is the original RegExp cloned from the original Nuxt.js configuration
- * files, with only the search for ".svg" files removed. Keep tabs on this in case
- * the core decides to add additional qualifiers to the pattern.
+ * files, with only the search for ".svg" files removed. Keep tabs on this in
+ * case the core decides to add additional qualifiers to the pattern.
  */
 const ORIGINAL_TEST = /\.(png|jpe?g|gif|svg|webp)$/;
 const REPLACEMENT_TEST = /\.(png|jpe?g|gif|webp)$/;
@@ -13,6 +13,7 @@ export default function Module(options) {
 /**
  * Perform the primary setup for the nuxt-svg module by removing and replacing
  * all of the rules that match ".svg" with the new one.
+ *
  * @param config The webpack configuration object to extend
  */
 function setup(config) {
@@ -20,12 +21,14 @@ function setup(config) {
 
   // Remove any original svg rules
   const svgRules = rules.filter(rule => rule.test.test(".svg"));
+
   svgRules.forEach(rule => {
     if (
       rule.test.source !== ORIGINAL_TEST.source &&
       rule.test.source !== REPLACEMENT_TEST.source
     )
       throw "nuxt-svg: Unexpected '.svg' rule in the webpack configuration";
+
     rule.test = REPLACEMENT_TEST;
   });
 
@@ -38,8 +41,13 @@ function setup(config) {
         loader: "vue-svg-loader",
         options: { svgo: false }
       },
-      { resourceQuery: /data/, loader: "url-loader" },
-      { loader: "file-loader" } // By default, always use file-loader
+      {
+        resourceQuery: /data/,
+        loader: "url-loader"
+      },
+      {
+        loader: "file-loader" // By default, always use file-loader
+      }
     ]
   };
 
