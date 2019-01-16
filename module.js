d9f72eab617ec679e4d488b8b6db7eaea1fa8923
Sam Holmes <samholmes1337@gmail.com>
2019-01-16 00:47:17 +0000

    Add custom rule support

--- module.js
+++ module.js
@@ -1,3 +1,5 @@
+const _ = require("lodash");
+
 /**
  * This is the original RegExp cloned from the original Nuxt.js configuration
  * files, with only the search for ".svg" files removed. Keep tabs on this in
@@ -6,8 +8,19 @@
 const ORIGINAL_TEST = /\.(png|jpe?g|gif|svg|webp)$/;
 const REPLACEMENT_TEST = /\.(png|jpe?g|gif|webp)$/;
 
+/**
+ * These are the default options that we want to use with the various loaders.
+ * They are overwritten as needed.
+ */
+const defaults = {
+  inline: { svgo: false },
+  data: {},
+  file: {}
+};
+
 export default function Module(options) {
-  this.extendBuild(setup);
+  options = _.merge(defaults, options);
+  this.extendBuild(config => setup(config, options));
 }
 
 /**
@@ -15,22 +28,25 @@ export default function Module(options) {
  * all of the rules that match ".svg" with the new one.
  *
  * @param config The webpack configuration object to extend
+ * @param options The Nuxt module options for this loader
  */
-function setup(config) {
+function setup(config, options) {
+  // Retrieve and remove original SVG rules
   const rules = config.module.rules;
-
-  // Remove any original svg rules
   const svgRules = rules.filter(rule => rule.test.test(".svg"));
 
-  svgRules.forEach(rule => {
+  // Search for the rule to modify
+  for (let rule of svgRules) {
     if (
       rule.test.source !== ORIGINAL_TEST.source &&
       rule.test.source !== REPLACEMENT_TEST.source
-    )
+    ) {
       throw "nuxt-svg: Unexpected '.svg' rule in the webpack configuration";
+    }
 
+    // Modify the rule
     rule.test = REPLACEMENT_TEST;
-  });
+  }
 
   // Create the custom SVG rule
   const rule = {
@@ -39,14 +55,16 @@ function setup(config) {
       {
         resourceQuery: /inline/,
         loader: "vue-svg-loader",
-        options: { svgo: false }
+        options: options.inline
       },
       {
         resourceQuery: /data/,
-        loader: "url-loader"
+        loader: "url-loader",
+        options: options.data
       },
       {
-        loader: "file-loader" // By default, always use file-loader
+        loader: "file-loader", // By default, always use file-loader
+        options: options.file
       }
     ]
   };
