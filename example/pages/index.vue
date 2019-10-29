0758160e3ff882f53eebf2b22611c8ada236e05b
Benjamin E <ben@elwyn.co>
2019-10-29 16:13:11 +0000

    add raw loader

--- example/pages/index.vue
+++ example/pages/index.vue
@@ -8,15 +8,25 @@
 
     <h3>Inline Components:</h3>
     <NuxtLogo />
+
+    <h3>Inline Raw</h3>
+    <div v-html="rawSvg" />
   </div>
 </template>
 
 <script>
 import NuxtLogo from '~/assets/nuxt.svg?inline'
+import nuxtLogoRaw from '~/assets/nuxt.svg?raw'
 
 export default {
   components: {
     NuxtLogo
+  },
+
+  data () {
+    return {
+      rawSvg: nuxtLogoRaw
+    }
   }
 }
 </script>
