281ac75bed06ed1f81c7117f1531a481d2e0e19d
Manuel Odelain <manuel@rezo-zero.com>
2023-01-08 21:56:13 +0100

    style: use Nuxt3 logo for Nuxt3 playground

--- playground/app.vue
+++ playground/app.vue
@@ -1,23 +1,65 @@
 <template>
-  <div class="list">
-    <div class="item">
-      <Logo />
+    <div class="root">
+      <div>
+        <h2>file-loader</h2>
+        <img src="~/assets/nuxt.svg" />
+      </div>
+
+      <div>
+        <h2>url-loader</h2>
+        <img src="~/assets/nuxt.svg?data" />
+      </div>
+
+      <div>
+        <h2>vue-svg-loader</h2>
+        <NuxtLogo />
+      </div>
+
+      <div>
+        <h2>raw-loader</h2>
+        <div v-html="rawNuxtLogo" />
+      </div>
+
+      <div>
+        <h2>svg-sprite-loader</h2>
+        <svg :viewBox="spriteNuxtLogo.viewBox">
+          <use :xlink:href="'#' + spriteNuxtLogo.id"></use>
+        </svg>
+      </div>
     </div>
-  </div>
 </template>
 
 <script setup>
-import Logo from '~/assets/nuxt.svg?inline'
+import NuxtLogo from '~/assets/nuxt.svg?inline'
+import rawNuxtLogo from "~/assets/nuxt.svg?raw"
+import spriteNuxtLogo from "~/assets/nuxt.svg?sprite";
 </script>
 
 <style>
-.list {
-  display: flex;
+body {
+  margin: 0;
+  padding: 0;
+  border: 0;
+
+  height: 100%;
+  background-color: #eee;
 }
 
-.item {
-  width: 25%;
-  border: 1px solid #ccc;
+.root {
+  font-family: sans-serif;
+
+  display: grid;
+  gap: 2px;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+}
+
+.root > div {
+  background-color: #fff;
+  padding: 20px;
+}
+
+h2 {
+  margin-top: 0;
 }
 </style>
 
